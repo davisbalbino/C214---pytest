8f639efd52a4f32cf2a46078ef5f3d75e8711f91
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-05-21 18:34:08 -0300

    :recycle: feat: new class atributes

--- task_manager/tasks/task.py
+++ task_manager/tasks/task.py
@@ -1,12 +1,19 @@
-# tasks/task.py
 class Task:
+    _id_counter = 1
+
     def __init__(self, title, description):
+        self.task_id = Task._id_counter
+        Task._id_counter += 1
         self.title = title
         self.description = description
         self.completed = False
+        self.assigned_to = None
 
     def mark_as_completed(self):
         self.completed = True
 
+    def assign_to(self, user):
+        self.assigned_to = user
+
     def __repr__(self):
-        return f"Task('{self.title}', '{self.description}', completed={self.completed})"
+        return f"Task('{self.title}', '{self.description}', completed={self.completed}, task_id={self.task_id})"
